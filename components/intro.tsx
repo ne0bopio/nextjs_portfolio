4e2c2ff9de4de2fc7e4ee6285ee12f9e7837bac8
pablo <pablo@test.com>
2026-03-01 22:42:55 -0500

    Update layout, intro component, and cv test

--- components/intro.tsx
+++ components/intro.tsx
@@ -4,7 +4,10 @@ import React from 'react'
 import Image from 'next/image'
 import Neobop from "@/public/neobop.png"
 import { motion } from "framer-motion"
-
+import Link from "next/link" 
+import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
+import { HiDownload } from 'react-icons/hi';
+import { FaGithubSquare } from 'react-icons/fa';
 
 export default function Intro() {
   return (
@@ -66,9 +69,34 @@ export default function Intro() {
             </span> 
         </motion.p>
 
-        <div>
+        <motion.div className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4
+        text-lg font-medium'
+                initial={{ opacity: 0, y: 100}}
+                animate={{ opacity: 1, y:0 }}
+                transition={{
+                    delay: 0.1,
+                }}
+        >
+            
             {/* Here we will add the buttons to download cv, contact us, and visit our github and linkelin */}
-        </div>
+        
+            <Link href="#contact" className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full 
+            outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition'> 
+            Contact me here <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition' />  
+            </Link>
+            <a href='/CV.pdf' download className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 
+            hover:scale-110 active:scale-105 transition cursor-pointer borderBlack'>Download CV <HiDownload className='opacity-70 group-hover:translate-y-1 transition' /> </a>
+
+            <a className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full
+             focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack">
+                <BsLinkedin />
+            </a>
+            <a className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15]
+             hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack ">
+                <FaGithubSquare />
+            </a>
+
+        </motion.div>
     </section>
   )
 }
